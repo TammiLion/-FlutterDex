c2f5d9e624e9e7c1173075179524f78007cb9edd
TammiLion <trp300@gmail.com>
2021-04-20 14:20:14 +0200

    Change package structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
-import 'dart:async';
-
-import 'package:dio/dio.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutterdex/di/injection.dart';
-import 'package:injectable/injectable.dart';
-import 'generated/locale_keys.g.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterdex/bottomsheet.dart';
-import 'package:flutterdex/data/Pokemon.dart';
-import 'package:flutterdex/network/api/PokeApi.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:logger/logger.dart';
-
-import 'network/data/NetworkResource.dart';
+import 'package:flutterdex/common/di/injection.dart';
+import 'package:injectable/injectable.dart';
 
 void main() async {
   configureDependencies(Environment.dev);
@@ -35,121 +24,25 @@ class MyApp extends StatelessWidget {
         localizationsDelegates: context.localizationDelegates,
         supportedLocales: context.supportedLocales,
         locale: context.locale,
-        home: RandomWords());
+        home: HomePage());
   }
 }
 
-class RandomWords extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  var networkResource = NetworkResource(Pokemon(name: "missingno"));
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(networkResource.when(
-            (Pokemon data) => data.name ?? "missingno",
-            loading: () => "loading",
-            error: (String? message) => message ?? "errorNull")),
+        title: Text("FlutterDex"),
       ),
       body: Container(
-        child: Column(
-          children: <Widget>[
-            TextButton(
-              child: Text(
-                LocaleKeys.msg.tr(),
-                style: GoogleFonts.roboto(),
-              ),
-              onPressed: () {
-                //doPokemonCall();
-                doRetrofitCall();
-              },
-            ),
-            BottomSheetshowingButton(),
-            Image.asset("assets/images/furniture_spritesheet.png"),
-          ],
-        ),
+        child: Image.asset("assets/images/furniture_spritesheet.png"),
       ),
     );
-  }
-
-  void writeTest() {
-    testStream();
-
-    var something = NetworkResource.error();
-    var something2 = NetworkResource(Pokemon(name: "Charmander"));
-    var something3 = NetworkResource.error("Error");
-
-    something2.when((Object data) => print('$data'),
-        loading: () => print('loading'),
-        error: (String? message) => print('$message'));
-  }
-
-  final log = Logger();
-
-  doRetrofitCall() {
-    writeTest();
-
-    setState(() {
-      networkResource = NetworkResource.loading();
-    });
-    final dio = Dio(); // Provide a dio instance
-    dio.options.headers["Demo-Header"] =
-        "demo header"; // config your dio headers globally
-    dio.interceptors.add(LogInterceptor(responseBody: true));
-    final client = PokeApi(dio);
-
-    client.getPokemonPage(0).then((value) => null);
-
-    client.getPokemonByName("ditto").then((it) {
-      log.i(it);
-      setState(() {
-        networkResource = NetworkResource(it);
-      });
-    }).catchError((Object obj) {
-      // non-200 error goes here.
-      switch (obj.runtimeType) {
-        case DioError:
-          // Here's the sample to get the failed response error code and message
-          final res = (obj as DioError).response;
-          log.e("Got error : ${res?.statusCode} -> ${res?.statusMessage}");
-          setState(() {
-            networkResource = NetworkResource.error("Error : ${obj.message}");
-          });
-          break;
-        default:
-      }
-    });
-  }
-
-  Stream<int> countStream(int max) async* {
-    for (int i = 0; i < max; i++) {
-      yield i;
-    }
-  }
-
-  Future<int> sumStream(Stream<int> stream) async {
-    int sum = 0;
-    await for (int value in stream) {
-      sum += value;
-    }
-    return sum;
-  }
-
-  void testStream() async {
-    /// Initialize a stream of integers 0-9
-    Stream<int> stream = countStream(10);
-
-    /// Compute the sum of the stream of integers
-    stream.listen((event) {
-      log.i('emitted: $event');
-    });
-    //int sum = await sumStream(stream);
-    /// Print the sum
-    //log.i('sum: $sum');
   }
 }
