5eab51b3ad18b4b0d28481b28879ff0015009b12
Tamara Roep <trp300@gmail.com>
2021-06-08 11:38:56 +0200

    Made a small start on .modular() extension for PlatformApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutterdex/common/di/injection.dart';
+import 'package:flutterdex/common/util/extensions.dart';
 import 'package:flutterdex/home/blocs/HomeBloc.dart';
 import 'package:flutterdex/modules/AppModule.dart';
 import 'package:injectable/injectable.dart';
@@ -25,16 +30,43 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget modularApp = Platform.isAndroid
+        ? _getMaterialApp(context)
+        : _getCupertinoApp(context);
+
     return MultiBlocProvider(
         providers: [BlocProvider<HomeBloc>(create: (_) => getIt<HomeBloc>())],
-        child: MaterialApp(
-                initialRoute: '/pokemon',
-                restorationScopeId: 'app',
-                localizationsDelegates: context.localizationDelegates,
-                supportedLocales: context.supportedLocales,
-                locale: context.locale)
-            .modular());
+        child: modularApp);
   }
-//TODO create modular extension for PlatformApp
+}
 
+Widget _getPlatformApp(BuildContext context) {
+  return PlatformApp(
+          initialRoute: "/pokemon",
+          restorationScopeId: 'app',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale)
+      .modular();
+}
+
+//TODO make it work if possible
+Widget _getMaterialApp(BuildContext context) {
+  return MaterialApp(
+          initialRoute: "/pokemon",
+          restorationScopeId: 'app',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale)
+      .modular();
+}
+
+Widget _getCupertinoApp(BuildContext context) {
+  return CupertinoApp(
+          initialRoute: "/pokemon",
+          restorationScopeId: 'app',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale)
+      .modular();
 }
